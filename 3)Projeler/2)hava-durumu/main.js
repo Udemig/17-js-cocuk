cf3e89550c512d6787ff74123b23a1116cd4fe42
tahaserdarmete <tserdarmete@gmail.com>
2026-04-04 15:33:02 +0300

    hava durumu projesi

---
+++ 3)Projeler/2)hava-durumu/main.js
@@ -0,0 +1,19 @@
+// Html'den elemanları Js kısmına çek
+var elements = {
+  form: document.querySelector("form"),
+  searchBtn: document.querySelector("#search-btn"),
+  fullDate: document.querySelector(".full-date"),
+  time: document.querySelector(".time"),
+  city: document.querySelector(".city"),
+  status: document.querySelector(".status"),
+  img: document.querySelector(".icon img"),
+  degree: document.querySelector(".degree span"),
+  feel: document.querySelector(".feel"),
+  hum: document.querySelector(".hum"),
+  wind: document.querySelector(".wind"),
+  sunrise: document.querySelector(".sunrise"),
+  sunset: document.querySelector(".sunset"),
+  max: document.querySelector(".max"),
+  min: document.querySelector(".min"),
+  locations: document.querySelector(".locations"),
+};
